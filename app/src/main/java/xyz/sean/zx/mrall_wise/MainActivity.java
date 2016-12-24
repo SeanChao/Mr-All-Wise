66ca39eda845f72bdbba7c1c6a11e97c65a4c636
Chao Sean <Chao Sean>
2016-12-24 21:34:18 +0800

    add some quotes&fix some bugs

--- app/src/main/java/xyz/sean/zx/mrall_wise/MainActivity.java
+++ app/src/main/java/xyz/sean/zx/mrall_wise/MainActivity.java
@@ -20,9 +20,15 @@ public class MainActivity extends AppCompatActivity {
     ListView mListView;
     private List<ZCYQuotes> quoteList = new ArrayList<ZCYQuotes>();
     private String content[] = {"做好课前准备","上课！（降调）","（……五分钟新课……）",
-            "上午第一节就犯困！","接下来我们处理作业","我把答案写在黑板上，同学们批改一下","（下来兜一圈）","对的题要打对er号！",
-            "一道一道地改！","我都兜了一圈了有的同学还没改完！（你慢你不行）","我来讲一下错的比较多的问题"
-
+            "上午第一节就犯困！","先说这个ding正作业","我一个礼拜就收这样的一次，有的同学就订正了3道题，你真的就只错了3道题？","接下来我们处理作业","我把答案写在黑板上，同学们批改一下","（下来兜一圈）","对的题要打对er号！",
+            "一道一道地改！","我都兜了一圈了有的同学还没改完！（你慢你不行）","我来讲一下错的比较多的问题","有些同学上来第一题就错！",
+            "我觉得没什么好讲的","第3题，错的举手！","将近有一半的同学错了","不举？你给我等着！","这个问题我强调了多少遍！",
+            "总gan觉到同学们对于我说过的问题不给予足够的重视！","这道题考试前一天刚讲过！","我没讲过别的方法，就讲过π-","不run许有并！我看谁有病！",
+            "下面看试卷","要养成写名字的好习惯，拿到卷子写上名字","下次谁不写名字就撕掉！","卷子叠叠好！","xxx字写写好","xxx你看看你，就没对几个！",
+            "xxx，你还会些啥？","我先设一个t","把这个式子展开，就能de（3声）到这样一件事情","cnr，把腿放下来","总gan觉到同学们对数学学习没有热情！",
+            "就说我们的课代表，能代表些啥？","我们没有形成数学学习的氛围","应该要有这样的领头羊","今天的作业，一课一练，就这样的两套题",
+            "有些同学去网上搜答案，你下次碰到了还得错！","你不明白为什么要这么做！","（跟1班的）差距一次比一次大！","迷信点说，是不是我们命中注定要这样",
+            "同学们写份反思","我的作业真的很少了","还搁那er玩游戏！","希望同学们能批评我"
             ,"The set is infinite" };
 
     @Override
@@ -33,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
-        initFruits(); // 初始化水果数据
+        initQuotes(); // 初始化水果数据
         QuoteAdapter adapter = new QuoteAdapter(MainActivity.this,
                 R.layout.quote_item, quoteList);
 
@@ -46,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "传义大法好！", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -105,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void initFruits() {
+    private void initQuotes() {
 
         for(int i = 0 ; i < content.length ; i++){
             newQoute(content[i]);
