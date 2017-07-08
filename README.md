48dfae42de85dfaa60cbd5aff86e2d4b96ec8bf4
SeanChao <seanchao0804@gmail.com>
2017-07-08 15:23:05 +0800

    test

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mr All Wise
+An Android app for all ChuanYi believers.
+[https://github.com/SeanChao/Mr-All-Wise/releases/latest](Click HERE to download the latesr version.)
---
+++ app/src/main/java/xz/sean/mr_all_wise/ZCYQuotes.java
@@ -0,0 +1,18 @@
+package xz.sean.mr_all_wise;
+
+/**
+ * Created by SeanC on 2016/12/23.
+ */
+
+public class ZCYQuotes {
+    private String quote;
+
+    public ZCYQuotes(String qoute){
+        this.quote = qoute;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+}
